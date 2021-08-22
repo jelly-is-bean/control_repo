32f7226e850d4dd24ec2ed09ee71e6402e56001b
jelly-is-bean <87625725+jelly-is-bean@users.noreply.github.com>
2021-08-22 16:57:16 +1200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,1 +1,15 @@
-
+class profile::ssh_server {
+  package { 'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorised_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => '',
+  }
+}
