bdc08c1dc5e53ce36f1d608aa8a6e5722eaf14bf
jelly-is-bean <87625725+jelly-is-bean@users.noreply.github.com>
2021-08-22 17:21:51 +1200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCozvWffVIu6MIuGsfnVh0ouHYpfxHpcBGxNQKGypGXmQ1PJSb68YB2l+T9XFnml6hU857sMAetdBAQ1mPYvMlADkwkCDKfeDmo6ecUej9//mDzuX4+LpPfsUJ8bY9/6C2kxRDXcJ9HLsN5STTJAoHzyalJ5PTGJOQN/zdM2WkDUfdrOwICH1ENSs4we+2bdEvpQueHAaY0VvvrubH0VeK5VOoUlmm8fIdV8V3wGpJ/pH60w8n1x2iRiy8P1GdcSKtYG2r0XGSw7wAamT3Z2CZPkFeSJcyKLqPZiBK85x6GhKGGosrYoG2fzZagpLRvHI7geSnr2Y6sGtwFBppYhosh',
   }
 }
